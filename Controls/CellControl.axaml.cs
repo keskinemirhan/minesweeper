bedd2a56e435fba33dc3465362e819a5c0af4857
keskinemirhan <keskinemirhan@gmail.com>
2024-11-03 11:37:23 +0300

    Add CellControl class

---
+++ Controls/CellControl.axaml.cs
@@ -0,0 +1,125 @@
+using Avalonia.Controls;
+using minesweeper.Models;
+using Avalonia.Media.Imaging;
+using Avalonia.Media;
+using Avalonia.Input;
+using Avalonia.Media.Immutable;
+using Avalonia.Platform;
+using System;
+
+namespace minesweeper.Controls;
+
+
+public partial class CellControl : UserControl, ICell
+{
+    public event EventHandler LeftButtonClicked;
+    public event EventHandler RightButtonClicked;
+    private bool _hasOpened;
+    public bool HasMine { get; set; }
+    public bool HasOpened
+    {
+        get
+        {
+            return this._hasOpened;
+        }
+        set
+        {
+            this._hasOpened = value;
+            if (value)
+            {
+                CellImg.IsVisible = false;
+                string imageUrl = "";
+                if (this.HasMine)
+                {
+                    imageUrl = "avares://minesweeper/Assets/mine.png";
+                }
+                else
+                {
+                    switch (this.NeighboringMineCount)
+                    {
+                        case 1:
+                            imageUrl = "avares://minesweeper/Assets/one.png";
+                            break;
+                        case 2:
+                            imageUrl = "avares://minesweeper/Assets/two.png";
+                            break;
+                        case 3:
+                            imageUrl = "avares://minesweeper/Assets/three.png";
+                            break;
+                        case 4:
+                            imageUrl = "avares://minesweeper/Assets/four.png";
+                            break;
+                        case 5:
+                            imageUrl = "avares://minesweeper/Assets/five.png";
+                            break;
+                        case 6:
+                            imageUrl = "avares://minesweeper/Assets/six.png";
+                            break;
+                        case 7:
+                            imageUrl = "avares://minesweeper/Assets/seven.png";
+                            break;
+                        case 8:
+                            imageUrl = "avares://minesweeper/Assets/eight.png";
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                if (!String.Equals(imageUrl, ""))
+                {
+                    var bitmap = new Bitmap(AssetLoader.Open(new Uri(imageUrl)));
+                    CellImg.IsVisible = true;
+                    CellImg.Source = bitmap;
+                }
+                CellButton.Background = new ImmutableSolidColorBrush(Color.FromRgb(230, 230, 230));
+                CellButton.IsEnabled = false;
+            }
+
+        }
+    }
+    private bool _isFlagged;
+    public bool IsFlagged
+    {
+        get
+        {
+            return this._isFlagged;
+        }
+        set
+        {
+            if (!this.HasOpened && value)
+            {
+                var imageUrl = "avares://minesweeper/Assets/flag.png";
+                var bitmap = new Bitmap(AssetLoader.Open(new Uri(imageUrl)));
+                CellImg.IsVisible = true;
+                CellImg.Source = bitmap;
+            }
+        }
+    }
+    public int RowPos { get; set; }
+    public int ColumnPos { get; set; }
+    public int NeighboringMineCount { get; set; }
+
+    public CellControl()
+    {
+        InitializeComponent();
+        CellButton.PointerPressed += (sender, e) =>
+        {
+            var pEvent = (PointerPressedEventArgs)e;
+            if (pEvent.GetCurrentPoint((Control)CellButton).Properties.IsRightButtonPressed)
+                RightButtonClicked?.Invoke(this, e);
+        };
+        CellButton.Click += (sender, e) => LeftButtonClicked?.Invoke(this, e);
+    }
+
+    public override string ToString()
+    {
+
+        return "[ Row: " + this.RowPos +
+               ", Column: " + this.ColumnPos +
+               ", Opened: " + this.HasOpened +
+               ", Has Mine: " + this.HasMine +
+               ", Flagged: " + this.IsFlagged +
+               ", Neighboring Mine Count: " + this.NeighboringMineCount + " ]";
+    }
+
+}
