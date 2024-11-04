e6fdbd56543ef55146236208a0190a5303024209
keskinemirhan <keskinemirhan@gmail.com>
2024-11-05 00:00:32 +0300

    Place start form in main window

--- Windows/MainWindow.axaml.cs
+++ Windows/MainWindow.axaml.cs
@@ -8,7 +8,19 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
-        var field = new FieldControl(30, 30, 10, this);
+        var startForm = new StartForm();
+        startForm.SubmitEvent += (sender, e) =>
+        {
+            this.DisplayGame(startForm.Username, startForm.GridSize, startForm.MineCount);
+        };
+        MainStackPanel.Children.Add(startForm);
+    }
+
+    public void DisplayGame(string username, int gridSize, int mineCount)
+    {
+        var field = new FieldControl(gridSize, gridSize, mineCount, this);
+        System.Console.WriteLine(gridSize);
+        MainStackPanel.Children.Clear();
         MainStackPanel.Children.Add(field);
     }
 }
